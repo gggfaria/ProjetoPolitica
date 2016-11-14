60c50db7d97374e54384957c9ac97b9aae8be350
Gabriel Faria <gggfaria@outlook.com>
2016-11-14 00:37:51 -0200

    adicionando comum.js

---
+++ web-app/js/comum.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Gabriel on 14/11/2016.
+ */
+
+function exibirSenha()
+{
+    $("#senha").attr("type", "text");
+}
+
+function esconderSenha()
+{
+    $("#senha").attr("type", "password");
+}
