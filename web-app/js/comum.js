d3427efaf62fb883ee7e1be482881b80b14ed20f
Felipe Guimarães <Felipe Guimares>
2016-12-12 17:36:26 -0200

    msg update

--- web-app/js/comum.js
+++ web-app/js/comum.js
@@ -2,85 +2,85 @@
  * Created by Gabriel on 14/11/2016.
  */
 
-function exibirSenha()
-{
+function exibirSenha() {
     $("[name='j_password']").attr("type", "text");
 }
 
-function esconderSenha()
-{
+function esconderSenha() {
     $("[name='j_password']").attr("type", "password");
 }
 
 
-
-
 //funcao de exibir mensagem de erro em modal
 function exibirMensagem(data) {
-    if(data.erro){
+    if (data.erro) {
 
 
         var erros = data.erro
         console.log(erros)
         $('#mensagemErros').empty()
-        for(var i=0; i < erros.length; i++){
+        for (var i = 0; i < erros.length; i++) {
             $.notify(erros[i], "error", {autoHideDelay: 15000});
             console.log(erros[i])
         }
 
         $.notify("Não foi possível concluir a operação", "error", {autoHideDelay: 15000});
 
-    }else {
+    } else {
         $.notify("Cadastro realizado com sucesso", "success");
         $("input[type=text]").val('')
         $('textarea').val('')
-        $('select').val('0')
+       // $('select').val('0')
         $('#modalForm').modal('hide')
     }
 }
 
 function exibirUpdate(data) {
-    if(data.erro){
+    if (data.erro) {
+
+
         var erros = data.erro
         console.log(erros)
         $('#mensagemErros').empty()
-        for(var i=0; i < erros.length; i++){
+        for (var i = 0; i < erros.length; i++) {
             $.notify(erros[i], "error", {autoHideDelay: 15000});
             console.log(erros[i])
+
         }
 
         $.notify("Não foi possível concluir a operação", "error", {autoHideDelay: 15000});
 
-    }else {
+    } else {
         $.notify("Dados atualizados com sucesso", "success");
         $("input[type=text]").val('')
         $('textarea').val('')
-        //$('select').val('0')
         $('#modalForm').modal('hide')
     }
 }
 
+
 //funcao de exibir mensagem de erro em modal
 function exibirMensagemModal(data) {
-    if(data.erro){
+    if (data.erro) {
         document.getElementById('mensagem').textContent = 'Não foi possível concluir a operação.'
-        document.getElementById('mensagem').setAttribute('class','alert alert-danger')
+        document.getElementById('mensagem').setAttribute('class', 'alert alert-danger')
 
         var divMensagemErro
         var erros = data.erro
         $('#mensagemErros').empty()
-        for(var i=0; i < erros.length; i++){
-            $('#mensagemErros').append("<div class='alert alert-danger'>"+erros[i]+"</div>"+"<br/>")
+        for (var i = 0; i < erros.length; i++) {
+            $('#mensagemErros').append("<div class='alert alert-danger'>" + erros[i] + "</div>" + "<br/>")
             console.log(erros[i])
         }
 
         $('#mensagemModal').modal('show')
-    }else {
+    } else {
         document.getElementById('mensagem').textContent = 'Cadastro realizado com sucesso.'
-        document.getElementById('mensagem').setAttribute('class','alert alert-success')
+        document.getElementById('mensagem').setAttribute('class', 'alert alert-success')
         $('#mensagemErros').empty()
         $('#mensagemModal').modal('show')
         $('input').val('')
+        $("input[type=text]").val('')
         $('select').val('0')
     }
 }
@@ -88,11 +88,11 @@ function exibirMensagemModal(data) {
 
 //funcao de exibir mensagem de sucesso adicionar pergunta, recebe o id da proposta
 function exibirMensagemAdicionarPergunta(data, id) {
-    if(data.erro){
+    if (data.erro) {
         var erros = data.erro
         console.log(erros)
         $('#mensagemErros').empty()
-        for(var i=0; i < erros.length; i++){
+        for (var i = 0; i < erros.length; i++) {
             $.notify(erros[i], "error", {autoHideDelay: 15000});
             console.log(erros[i])
         }
@@ -100,13 +100,42 @@ function exibirMensagemAdicionarPergunta(data, id) {
         $.notify("Não foi possível concluir a operação", "error", {autoHideDelay: 15000});
 
 
-
-    }else {
+    } else {
         $.notify("Pergunta enviada com sucesso", "success");
-        $('input').val('')
+        $('textarea').val('')
+        $("input[type=text]").val('')
         atualizarPerguntas(id)
 
     }
+}
+
+
+//funcao de exibir mensagem de sucesso adicionar pergunta, recebe o id da proposta
+function exibirMensagemGenerica(data, mensagemSucesso, isLimpar) {
+    if (data.erro) {
+        var erros = data.erro
+        console.log(erros)
+        $('#mensagemErros').empty()
+        for (var i = 0; i < erros.length; i++) {
+            $.notify(erros[i], "error", {autoHideDelay: 15000});
+            console.log(erros[i])
+        }
+
+        $.notify("Não foi possível concluir a operação", "error", {autoHideDelay: 15000});
+
+
+    } else {
+        $.notify(mensagemSucesso, "success");
+        if (isLimpar) {
+            limparCampos()
+        }
+
+    }
+}
+
+function limparCampos() {
+    $('input').val('')
+    $('textarea').val('')
 }
 
 
